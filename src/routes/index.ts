814cd51528ee1c311dfbcd28873be034d927226d
luis gonzalez <luisgonzalez18997@gmail.com>
2021-06-04 13:06:04 -0400

    add document routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,8 @@ declare module 'express' {
     }
   }
 import { Request, Response } from 'express';
-import { ImportData } from '../controllers';
+import { getOrder, ImportData } from '../controllers';
+import { getOrders, getCustomer } from '../controllers/index';
 
 const routes = (router:any) => {
     /**
@@ -51,6 +52,122 @@ const routes = (router:any) => {
         let data = null
         try {
             data = await ImportData(req.files.document);
+            
+        } catch (error) {
+            status= 400
+            result = {
+            success: false,
+            message: error
+            }
+        }
+        res.status(status).send({...result, data})
+    })
+
+    /**
+     * @swagger
+     * /order/{code}:
+     *  get:
+     *    tags:
+     *      - Import
+     *    description: get  data of by order code
+     *    parameters:
+     *      - name: type
+     *        in: path
+     *        required: true
+     *        description: code order
+     *        schema:
+     *          type: string
+     *    responses:
+     *      200:
+     *        description: OK
+     *      400:
+     *        description: BAD REQUEST
+     */
+     
+     router.get('/order/:code', async(req: Request, res: Response) => {
+        let status = 200
+        let result = {
+        success: true,
+        message: 'Get Order found'
+        }
+        let data = null
+        try {
+            data = await getOrder(req.params.code);
+            
+        } catch (error) {
+            status= 400
+            result = {
+            success: false,
+            message: error
+            }
+        }
+        res.status(status).send({...result, data})
+    })
+
+    /**
+     * @swagger
+     * /orders:
+     *  get:
+     *    tags:
+     *      - Import
+     *    description: get data all by order
+     *    responses:
+     *      200:
+     *        description: OK
+     *      400:
+     *        description: BAD REQUEST
+     */
+     
+    router.get('/orders', async(req: Request, res: Response) => {
+        let status = 200
+        let result = {
+        success: true,
+        message: 'Get Order found'
+        }
+        let data = null
+        try {
+            data = await getOrders();
+            
+        } catch (error) {
+            status= 400
+            result = {
+            success: false,
+            message: error
+            }
+        }
+        res.status(status).send({...result, data})
+    })
+
+    /**
+     * @swagger
+     * /customer/{code}:
+     *  get:
+     *    tags:
+     *      - Import
+     *    description: get  data of by customer code
+     *    parameters:
+     *      - name: type
+     *        in: path
+     *        required: true
+     *        description: code customer
+     *        schema:
+     *          type: string
+     *    responses:
+     *      200:
+     *        description: OK
+     *      400:
+     *        description: BAD REQUEST
+     */
+     
+     router.get('/customer/:code', async(req: Request, res: Response) => {
+        let status = 200
+        let result = {
+        success: true,
+        message: 'Get Order found'
+        }
+        let data = null
+        try {
+            data = await getCustomer(req.params.code);
             
         } catch (error) {
             status= 400
